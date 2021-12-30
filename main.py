8030dfe7e2be62e808a6445b6c7ba587583869ce
Qusdy <92876836+Qusdy@users.noreply.github.com>
2021-12-30 16:08:15 +0300

    подправил код
    изменил класс game на функцию game

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from constants import *
 import pygame
-from game import Game
+from game import game
 from start_menu import start_menu
 
 running = True
 pygame.init()
 
 start_menu()
-game = Game()
+game()
